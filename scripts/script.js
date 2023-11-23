67b497304939d4cb7d1b3cf977393d6c8d954748
Simon Kutsche <simon.kutsche@gmx.de>
2023-11-23 00:15:43 -0500

    media queries

--- scripts/script.js
+++ scripts/script.js
@@ -36,3 +36,31 @@ window.addEventListener('scroll', () => {
         header.classList.remove('header-solid');
     }
 });
+
+
+// Burger menu
+function toggleMenu() {
+    const header = document.querySelector('.header');
+    header.classList.toggle('burger-menu-active');
+    }
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwA8_m-VdiyuTkQuEgENd5zNczg7N9yzeIMm86sKMtMw0tw0Kbx6KOx1Ko-54c5krAR/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, {
+        method: 'POST',
+        body: new FormData(form),
+    })
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()  
+    })
+    .catch(error => console.error('Error!', error.message))
+})
